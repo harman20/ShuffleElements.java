656a57ffffd926797b2e6933a5af22e0aa9e9736
Harmanjit Singh <57390891+harman20@users.noreply.github.com>
2020-08-03 16:54:57 +0530

    Add files via upload

---
+++ ShuffleElements.java
@@ -0,0 +1,14 @@
+import java.util.*;
+  public class ShuffleElements{
+  public static void main(String[] args) {
+  List<String> list_Strings = new ArrayList<String>();
+  list_Strings.add("Red");
+  list_Strings.add("Green");
+  list_Strings.add("Orange");
+  list_Strings.add("White");
+  list_Strings.add("Black");
+  System.out.println("List before shuffling:\n" + list_Strings);  
+  Collections.shuffle(list_Strings);
+  System.out.println("List after shuffling:\n" + list_Strings); 
+ }
+}
